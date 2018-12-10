4a5c64d9b51e395629d0767d7d24873a480e729e
Dany <danie.hdez03@gmail.com>
2018-12-09 23:00:23 -0600

    Problems + Leaderboards changes, some linter fixes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-const urlUser = "https://hackersarena-224603.appspot.com/users/";
-let uu = "Franco1010"
+const urlUser = 'https://hackersarena-224603.appspot.com/users/';
+const uu = 'Franco1010';
 
- function obtenerDatosUsuario(){
- fetch(urlUser.concat(uu))
- .then((resp) => resp.json() )
- .then(function(data) {
-   let user;
-   user = data.data;
-  // console.log(user);
-   userName.innerHTML = `${user.name}`;
-   userAlias.innerHTML = `${user.alias}`;
-   userAliasA.innerHTML = userAlias.innerHTML;
-   userUniversity.innerHTML = `${user.university}`;
-   userCountry.innerHTML = `${user.country}`;
-   userPoints.innerHTML = `${user.score}`;
-   userResolved.innerHTML = `${user.score}`;
-   userTrd.innerHTML = userResolved.innerHTML;
-   return;
- })
- .catch(function(error) {
-   console.log(error)
- });
+function obtenerDatosUsuario() {
+  fetch(urlUser.concat(uu))
+    .then(resp => resp.json())
+    .then((data) => {
+      const user = data.data;
+      // console.log(user);
+      userName.innerHTML = `${user.name}`;
+      userAlias.innerHTML = `${user.alias}`;
+      userAliasA.innerHTML = userAlias.innerHTML;
+      userUniversity.innerHTML = `${user.university}`;
+      userCountry.innerHTML = `${user.country}`;
+      userPoints.innerHTML = `${user.score}`;
+      userResolved.innerHTML = `${user.score}`;
+      userTrd.innerHTML = userResolved.innerHTML;
+      return;
+    })
+    .catch((error) => {
+      console.log(error)
+    });
 }
--- js/problems.js
+++ js/problems.js
@@ -1,33 +1,39 @@
-const url = 'https://hackersarena-224603.appspot.com/solutions';
-// The data we are going to send in our request
-let data = {
-  aliasUser: "Naekkeoya",
-  idProblem: 2,
-  idLanguage: 2,
-  output: "Just trying!",
-};
+const urlP = './submitSolutions.html#';
 
-let header = new Headers();
-header.append('Content-Type', 'application/x-www-form-urlencoded');
-header.append('token', '$2b$10$tOUvGv8/nVkGky9T4nPn8O1N1y5VVEiPtQyuTGt1Tqd1dSTux4G4O');
-// Create our request constructor with all the parameters we need
-var request = new Request(url, {
-    method: 'POST',
-    body: data,
-    bodyUsed: true,
-    headers: header,
-});
-request.body = data;
-console.log(request.headers.get('Content-Type'));
-console.log(request.headers.get('token'));
-console.log(request.body);
-console.log(request);
-
-function sendData(){
- fetch(request)
-  .then(function() {
-    console.log("Try");
-
-      // Handle response we get from the API
-  });
+function createNode(element) {
+  return document.createElement(element);
 }
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+function obtenerDatos() {
+  fetch('https://hackersarena-224603.appspot.com/problems')
+    .then(resp => resp.json())
+    .then((data) => {
+      const problems = data.data;
+      return problems.map((problem) => {
+        const ulPrb = document.getElementById('ulProblems');
+
+        for (let i = 0; i < problem.length; i++) {
+          const liPrb = createNode('li');
+          const spanPrb = createNode('span');
+          const aPrb = createNode('a');
+          aPrb.href = urlP.concat(problem[i].id);
+          spanPrb.innerHTML = `${problem[i].title}`;
+          // Appending items
+          append(aPrb, spanPrb);
+          append(liPrb, aPrb);
+          append(ulPrb, liPrb);
+        }
+        return;
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+}
+
+
+document.addEventListener('onload', obtenerDatos(), true);
