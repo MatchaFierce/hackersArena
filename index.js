a1944124f1c3d4f57c438181f8c654ec711b44c2
MatchaFierce <cesarvaldez.work@gmail.com>
2018-12-11 06:14:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ router.get('/', (req, res) => {
 
 
 const PORT = process.env.PORT || '8080';
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port: ${process.env.PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
